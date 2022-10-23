adb446680f3e9c038b5d313e15e244ac673494be
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-10-23 07:20:08 -0700

    Adds styled form to PeaksNew.jsx wit onSubmit to call handleSubmit method

--- src/PeaksNew.jsx
+++ src/PeaksNew.jsx
@@ -1,13 +1,57 @@
 import axios from "axios";
 
 export function PeaksNew(props) {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const params = new FormData(event.target);
+    event.target.reset();
+  };
 
-const handleSubmit = (event) => {
-  event.preventDefault();
-  const params = new FormData(event.target);
-  event.target.reset();
-}
-
-return ();
-
+  return (
+    <div>
+      <h1>New Product</h1>
+      <form className="row g-3" onSubmit={handleSubmit}>
+        <div className="col-md-6">
+          <label for="name" className="form-label">
+            Name
+          </label>
+          <input type="text" id="title" name="name" className="form-control"></input>
+        </div>
+        <div className="col-md-6">
+          <label for="image" className="form-label">
+            Peak Image
+          </label>
+          <input type="text" id="image" name="image" className="form-control"></input>
+        </div>
+        ]
+        <div className="col-md-6">
+          <label for="location" className="form-label">
+            Location
+          </label>
+          <input type="text" id="location" name="location" className="form-control"></input>
+        </div>
+        <div className="col-md-6">
+          <label for="elevation" className="form-label">
+            Elevation(ft)
+          </label>
+          <input type="number" id="elevation" name="elevation" className="form-control"></input>
+        </div>
+        <div className="col-md-12">
+          <label for="description" className="form-label">
+            Description
+          </label>
+          <input type="text" id="description" name="description" className="form-control"></input>
+        </div>
+        <div className="col-md-12">
+          <label for="description" className="form-label">
+            Description
+          </label>
+          <input type="text" id="description" name="description" className="form-control"></input>
+        </div>
+        <div className="col-12">
+          <input type="submit" id="submit" name="submit" className="btn btn-primary"></input>
+        </div>
+      </form>
+    </div>
+  );
 }
