fe7edaa74150a299dd1fe012a314992dc0028ffb
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-10-22 18:54:43 -0700

    Adds show and hide functions for modal via props in Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,13 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
+import { Modal } from "./Modal";
 import { PeaksIndex } from "./PeaksIndex";
+import { PeaksShow } from "./PeaksShow";
 
 export function Home() {
   const [peaks, setPeaks] = useState([]);
+  const [isPeakVisible, setIsPeakVisible] = useState(false);
+  const [currentPeak, setCurrentPeak] = useState({});
 
   const handleIndexPeaks = () => {
     axios.get("http://localhost:3000/peaks.json").then((response) => {
@@ -12,11 +16,24 @@ export function Home() {
     });
   };
 
+  const handleShowPeak = (peak) => {
+    setIsPeakVisible(true);
+    setCurrentPeak(peak);
+  };
+
+  const handleHidePeak = () => {
+    setIsPeakVisible(false);
+  };
+
   useEffect(handleIndexPeaks, []);
 
   return (
     <div>
-      <PeaksIndex peaks={peaks} />
+      <PeaksIndex peaks={peaks} onSelect={handleShowPeak} />
+      <Modal>
+        <PeaksShow />
+        <p>TEST</p>
+      </Modal>
       <div id="peaks-new">
         <h1>New Peak</h1>
       </div>
---
+++ src/PeaksShow.jsx
@@ -0,0 +1,7 @@
+export function PeaksShow(props) {
+  return (
+    <div>
+      <h2>{props.peak.name}</h2>
+    </div>
+  );
+}
