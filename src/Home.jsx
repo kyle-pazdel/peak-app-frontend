82633ed34f16df3b44177a943331ff57dda67297
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-10-22 17:50:40 -0700

    Adds setPeaks to index function in Home.jsx, renders peaks with map method, and renders peaks's images with map function

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,14 +6,17 @@ export function Home() {
   const [peaks, setPeaks] = useState([]);
 
   const handleIndexPeaks = () => {
-    axios.get("http://localhost:3000/products.json").then((response) => {
+    axios.get("http://localhost:3000/peaks.json").then((response) => {
       console.log(response.data);
+      setPeaks(response.data);
     });
   };
 
+  useEffect(handleIndexPeaks, []);
+
   return (
     <div>
-      <PeaksIndex />
+      <PeaksIndex peaks={peaks} />
       <div id="peaks-new">
         <h1>New Peak</h1>
       </div>
--- src/PeaksIndex.jsx
+++ src/PeaksIndex.jsx
@@ -1,12 +1,19 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-
-export function PeaksIndex() {
+export function PeaksIndex(props) {
   return (
-    <div>
-      <div id="peaks-index">
-        <h1>All Peaks</h1>
-      </div>
+    <div id="peaks-index">
+      <h1>All Peaks</h1>
+      {props.peaks.map((peak) => (
+        <div className="peaks" key={peak.id}>
+          {peak.images.map((image) => (
+            <img src={image.url} alt={peak.description} />
+          ))}
+          <h2>{peak.name}</h2>
+          <p>{peak.description}</p>
+          <p>{peak.location}</p>
+          <p>{peak.elevation}</p>
+        </div>
+      ))}
+      <p>Peaks Go Here!</p>
     </div>
   );
 }
