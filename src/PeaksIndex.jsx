98e581e981499ce22f6d1cb40e68e24518cf0aab
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-10-22 18:00:19 -0700

    Adds bootstrap card styling to PeaksIndex.jsx

--- src/PeaksIndex.jsx
+++ src/PeaksIndex.jsx
@@ -1,16 +1,20 @@
 export function PeaksIndex(props) {
   return (
-    <div id="peaks-index">
+    <div id="peaks-index" className="row">
       <h1>All Peaks</h1>
       {props.peaks.map((peak) => (
-        <div className="peaks" key={peak.id}>
-          {peak.images.map((image) => (
+        <div className="peaks bg-dark card m-1" key={peak.id} style={{ width: "18rem" }}>
+          {/* {peak.images.map((image) => (
             <img src={image.url} alt={peak.description} />
-          ))}
-          <h2>{peak.name}</h2>
-          <p>{peak.description}</p>
-          <p>{peak.location}</p>
-          <p>{peak.elevation}</p>
+          ))} */}
+          <div className="card-body bg-custom-1">
+            <img src={peak.images[0].url} className="card-img-top" alt={peak.description} />
+            <h2 className="card-title bg-custom-1">{peak.name}</h2>
+            <p className="card-text bg-custom-1">{peak.description}</p>
+            <p>{peak.location}</p>
+            <p>{peak.elevation}</p>
+            <button className="btn btn-primary">MoreInfo</button>
+          </div>
         </div>
       ))}
     </div>
