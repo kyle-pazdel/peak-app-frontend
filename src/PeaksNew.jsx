d1704f555c378b8fafe53716039310e934ac790b
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-10-23 07:12:42 -0700

    Adds PeaksNew.jsx file with handSubmit function

---
+++ src/PeaksNew.jsx
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+export function PeaksNew(props) {
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+  const params = new FormData(event.target);
+  event.target.reset();
+}
+
+return ();
+
+}
