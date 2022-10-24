f57857ed94c6dea4ceb7e5a2884d9b185444433a
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-10-24 07:03:18 -0700

    Adds handleSubmit function and form to PeaksShow for updates to peak information

--- src/PeaksShow.jsx
+++ src/PeaksShow.jsx
@@ -1,5 +1,12 @@
 export function PeaksShow(props) {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const params = new FormData(event.target);
+    event.target.reset();
+  };
+
   return (
+    // Carousel with all peak images and peak data
     <div id="peaks-show">
       <div id="carouselExampleControls" className="carousel slide mt-5 mb-3" data-bs-ride="carousel">
         <div className="carousel-inner">
@@ -32,6 +39,46 @@ export function PeaksShow(props) {
       <p>{props.peak.description}</p>
       <p>{props.peak.location}</p>
       <p>{props.peak.elevation} ft</p>
+    </div>
+
+    // Form for updating peak information
+    <div>
+      <h1>New Peak</h1>
+      <form className="row g-3" onSubmit={handleSubmit}>
+        <div className="col-md-6">
+          <label for="name" className="form-label">
+            Name
+          </label>
+          <input type="text" id="title" name="name" className="form-control"></input>
+        </div>
+        <div className="col-md-6">
+          <label for="image" className="form-label">
+            Peak Image
+          </label>
+          <input type="text" id="image" name="image" className="form-control"></input>
+        </div>
+        <div className="col-md-6">
+          <label for="location" className="form-label">
+            Location
+          </label>
+          <input type="text" id="location" name="location" className="form-control"></input>
+        </div>
+        <div className="col-md-6">
+          <label for="elevation" className="form-label">
+            Elevation(ft)
+          </label>
+          <input type="number" id="elevation" name="elevation" className="form-control"></input>
+        </div>
+        <div className="col-md-12">
+          <label for="description" className="form-label">
+            Description
+          </label>
+          <input type="text" id="description" name="description" className="form-control"></input>
+        </div>
+        <div className="col-12">
+          <input type="submit" id="submit" name="submit" className="btn btn-primary"></input>
+        </div>
+      </form>
     </div>
   );
 }
